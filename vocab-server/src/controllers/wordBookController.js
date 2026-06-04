9aabdea5b8d98181433d906e7392ae456d51a4b5
ShawnYoung <2794129267@qq.com>
2026-06-04 18:36:53 +0800

    fix: order word books by id ASC so primary school appears first

--- vocab-server/src/controllers/wordBookController.js
+++ vocab-server/src/controllers/wordBookController.js
@@ -3,7 +3,7 @@ const { success, error } = require('../utils/response');
 
 async function getAllWordBooks(req, res) {
   try {
-    const books = await WordBook.findAll();
+    const books = await WordBook.findAll({ order: [['id', 'ASC']] });
     return res.json(success(books, '获取单词书成功'));
   } catch (err) {
     return res.json(error('获取单词书失败', err));
